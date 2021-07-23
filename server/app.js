513743acda034e1d1afc7f35502f1eb48a60b0f9
Anastasiia Filimonova <2850025@gmail.com>
2021-07-23 08:25:20 +0300

    task 4.1 part 2

--- server/app.js
+++ server/app.js
@@ -4,61 +4,25 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const PORT = 3001;
-const User = require ('./model/mongo/User')
-const Post = require ('./model/mongo/Post')
-const Comment = require ('./model/mongo/Comment')
+const UserController = require('./controller/User')
+const PostController = require('./controller/Post')
+const CommentController = require('./controller/Comment')
 
 app.use(cors());
 
 app.use(express.static('public'));
 
-
 // users
-app.get('/users', async (req, res) => {
-  res.json({
-      items: await User.find()
-  });
-});
-
-app.get('/users/:id', async (req, res) => {
-    res.json({
-        item: await User.findOne({id:req.params.id})
-    });
-  });
+app.get('/users', UserController.list);
+app.get('/users/:id', UserController.getById);
 
 // posts
-app.get('/posts', async (req, res) => {
-  const criteria={}
-  if (req.query.userId){
-    criteria.userId=req.query.userId
-  }
-  res.json({
-      items: await Post.find(criteria)
-  });
-});
+app.get('/posts', PostController.list);
+app.get('/posts/:id', PostController.getById);
 
-app.get('/posts/:id', async (req, res) => {
-    res.json({
-        item: await Post.findOne({id:req.params.id})
-    });
-  });
-
-  // comments
-app.get('/comments', async (req, res) => {
-  const criteria={}
-  if (req.query.postId){
-    criteria.postId=req.query.postId
-  }
-  res.json({
-      items: await Comment.find(criteria)
-  });
-});
-
-app.get('/comments/:id', async (req, res) => {
-    res.json({
-        item: await Comment.findOne({id:req.params.id})
-    });
-  });
+// comments
+app.get('/comments', CommentController.list);
+app.get('/comments/:id', CommentController.getById);
 
 app.listen(PORT, () => {
   console.log(`Server app listening at http://localhost:${PORT}`);
