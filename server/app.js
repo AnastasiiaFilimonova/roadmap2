24f024f64dc66944d1d4ef629610dbe73b690cc2
Anastasiia Filimonova <2850025@gmail.com>
2021-09-10 07:31:09 +0300

    old server file remove

--- server/app.js
+++ server/app.js
@@ -4,26 +4,11 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const PORT = 3001;
-const UserController = require('./controller/User')
-const PostController = require('./controller/Post')
-const CommentController = require('./controller/Comment')
 const MovieController = require('./controller/Movie')
 
 app.use(cors());
 
 app.use(express.static('public'));
-
-// users
-app.get('/users', UserController.list);
-app.get('/users/:id', UserController.getById);
-
-// posts
-app.get('/posts', PostController.list);
-app.get('/posts/:id', PostController.getById);
-
-// comments
-app.get('/comments', CommentController.list);
-app.get('/comments/:id', CommentController.getById);
 
 // movies
 app.get('/movies', MovieController.list);
